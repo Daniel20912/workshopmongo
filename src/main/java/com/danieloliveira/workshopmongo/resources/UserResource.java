c372a11d3b74423342335ebebb5aa7a3222f25bc
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2025-01-20 16:00:59 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/danieloliveira/workshopmongo/resources/UserResource.java
+++ src/main/java/com/danieloliveira/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.danieloliveira.workshopmongo.resources;
 
+import com.danieloliveira.workshopmongo.domain.Post;
 import com.danieloliveira.workshopmongo.domain.User;
 import com.danieloliveira.workshopmongo.dto.UserDTO;
 import com.danieloliveira.workshopmongo.services.UserService;
@@ -55,6 +56,12 @@ public class UserResource {
         obj.setId(id);
         userService.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
 
