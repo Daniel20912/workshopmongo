a094c0268bbe85d1f2d5b3d0cad40f144ba916e3
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2025-01-20 15:36:59 -0300

    Projeção dos dados do Ator com DTo

--- src/main/java/com/danieloliveira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/danieloliveira/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.danieloliveira.workshopmongo.config;
 
 import com.danieloliveira.workshopmongo.domain.Post;
 import com.danieloliveira.workshopmongo.domain.User;
+import com.danieloliveira.workshopmongo.dto.AuthorDTO;
 import com.danieloliveira.workshopmongo.repositories.PostRepository;
 import com.danieloliveira.workshopmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,11 +34,12 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, LocalDate.parse("21/03/2018", dtf), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, LocalDate.parse("23/03/2018", dtf), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        // é preciso adicionar o user ao banco de dados antes de criar um AuthorDTO dele, pois ele só vai receber um Id após ser adicionado ao banco de dados
+        Post post1 = new Post(null, LocalDate.parse("21/03/2018", dtf), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, LocalDate.parse("23/03/2018", dtf), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
