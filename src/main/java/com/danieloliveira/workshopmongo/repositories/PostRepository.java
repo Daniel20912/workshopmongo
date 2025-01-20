d43d2a436e66a67ddf2165bdac4cbcf346d825e5
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2025-01-20 18:07:13 -0300

    Consulta com vários critérios

--- src/main/java/com/danieloliveira/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/danieloliveira/workshopmongo/repositories/PostRepository.java
@@ -4,6 +4,7 @@ import com.danieloliveira.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
@@ -19,4 +20,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     // query method que busca o post com o título passado
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
