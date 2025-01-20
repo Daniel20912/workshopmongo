7471a095863d6c247edf5fb081a6298715832439
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2025-01-20 17:26:46 -0300

    Consulta simples com @Query

--- src/main/java/com/danieloliveira/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/danieloliveira/workshopmongo/repositories/PostRepository.java
@@ -2,10 +2,20 @@ package com.danieloliveira.workshopmongo.repositories;
 
 import com.danieloliveira.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    /*
+       Consulta personalizada usando a anotação @Query
+       'title': Campo no documento MongoDB que será pesquisado
+       $regex: ?0: Usa uma expressão regular para buscar correspondências no campo title. O marcador "?0" refere-se ao primeiro parâmetro passado para o metodo searchTitle. No caso, será a variável text.
+       $options: 'i': Indica que a busca deve ser case-insensitive (ignora diferenças entre maiúsculas e minúsculas).
+     */
+    @Query("{'title' :  {$regex: ?0, $options: 'i'}}")
+    List<Post> searchTitle(String text);
 
     // query method que busca o post com o título passado
     List<Post> findByTitleContainingIgnoreCase(String text);
